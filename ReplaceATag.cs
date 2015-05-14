13719a26772d9db3b1f306f6c70a037495e6a27f
Ivo Malakov <ivo.malakov85@gmail.com>
2015-05-14 22:44:05 +0300

    Add source codes to firs two problems for the homework

---
+++ ReplaceATag.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class ReplaceATag
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter a your string here: ");
+        string input = Console.ReadLine();
+
+        string pattern = @"(?<tag><a href=(?<link>http:\/\/\w+.\w+)>(?<text>\w+)<\/a>)";
+        Match match = Regex.Match(input, pattern);
+        string replace = "[URL href=" + match.Groups[2] + "]" + match.Groups[3] + "[/URL]";
+        string result = Regex.Replace(input, pattern, replace);
+        string quotes = @"""";
+        result = Regex.Replace(result, quotes, "");
+
+        Console.WriteLine();
+        Console.WriteLine("Your string after the replacement looks like that: ");
+        Console.WriteLine();
+        Console.WriteLine(result);
+    }
+}
---
+++ SeriesOfLetters.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class SeriesOfLetters
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter a your string here: ");
+        Console.WriteLine();
+        string input = Console.ReadLine();
+
+        string pattern = @"([a-z]|[A-Z])\1+";
+        string replace = "$1";
+        string result = Regex.Replace(input, pattern, replace);
+
+        Console.WriteLine("Your string after the replacement looks like that: ");
+        Console.WriteLine();
+        Console.WriteLine(result);
+    }
+}
