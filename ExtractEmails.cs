0f91d178cdd64ded6cfb874f592baf86605fcb8f
Ivo Malakov <ivo.malakov85@gmail.com>
2015-05-15 18:51:36 +0300

    Add source codes for Problem3. ExtractEmails and Problem4 Sentence Extractor

---
+++ ExtractEmails.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class ExtractEmails
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter your text here: ");
+        Console.WriteLine();
+        string text = Console.ReadLine();
+
+        string pattern = @"(?<user>[a-z][\w.-]+)@(?<host>[a-z][\w-]+\.[a-z]+)";
+        MatchCollection matches = Regex.Matches(text, pattern);
+
+        Console.WriteLine("Your text contains the following emails in himself: ");
+        Console.WriteLine();
+
+        foreach (Match match in matches)
+        {
+            Console.WriteLine(match);
+        }
+    }
+}
---
+++ SetenceExtractor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class SetenceExtractor
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter your keyword here: ");
+        string keyword = Console.ReadLine();
+        Console.WriteLine();
+        Console.WriteLine("Enter your text here: ");
+        Console.WriteLine();
+        string text = Console.ReadLine();
+
+        string pattern = @"[A-Z][\w\W]*?\b" + keyword + @"\b[\w\W]*?[.!?]";
+        MatchCollection matches = Regex.Matches(text, pattern);
+
+        Console.WriteLine("Your text contains the following setences in himself: ");
+        Console.WriteLine();
+
+        foreach (Match match in matches)
+        {
+            Console.WriteLine(match);
+        }
+    }
+}
